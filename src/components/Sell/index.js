5ef8c6dd105c67e4e8f2add0d25b556843ad2fc5
lij313 <lij313@mcmaster.ca>
2021-07-15 14:11:49 -0600

    update styling for sell page

--- src/components/Sell/index.js
+++ src/components/Sell/index.js
@@ -16,14 +16,14 @@ import detectEthereumProvider from '@metamask/detect-provider';
 
 import ProgressBar from "../Progress_bar";
 import { validate } from 'uuid';
-function ElogDateTime({selected, handleChange}){
+function ElogDateTime({ selected, handleChange }) {
     const [date, setDate] = useState(selected && selected.split(" ")[0]);
     const [time, setTime] = useState(selected && selected.split(" ")[1]);
     const dateRef = useRef(null);
     const timeRef = useRef(null);
 
     useEffect(() => {
-        if(!date || !time) return;
+        if (!date || !time) return;
     }, [date, time]);
 
     function _handleChange(e) {
@@ -31,17 +31,17 @@ function ElogDateTime({selected, handleChange}){
         const value = e.target.value;
         const elid = e.target.id;
         let newStr;
-    
+
         if ("elogdate" === elid) {
-          setDate(value);
-          newStr = new String("").concat(value||"0000-00-00", " ", time||"00:00");
+            setDate(value);
+            newStr = new String("").concat(value || "0000-00-00", " ", time || "00:00");
         } else if ("elogtime" === elid) {
-          setTime(value);
-          newStr = new String("").concat(date||"0000-00-00", " ", value||"00:00");
+            setTime(value);
+            newStr = new String("").concat(date || "0000-00-00", " ", value || "00:00");
         }
         handleChange(newStr);
     }
-    
+
     return (
         <div className='auction-date-time'>
             <div className='auction-date'>
@@ -63,10 +63,10 @@ function ElogDateTime({selected, handleChange}){
                 />
             </div>
         </div>
-      );
+    );
 }
 
-function setPriceErrorMsg(){
+function setPriceErrorMsg() {
     return <p>Invalid start price.</p>
 }
 
@@ -88,16 +88,16 @@ function Sell() {
     const [chosenCharity, setChosenCharity] = useState("");
     const [schemaName, setSchemaName] = useState("");
     const [tokenPrice, setTokenPrice] = useState(-1);
-  
+
     /**
      * Uses React effects perform one-time actions.
      *
      * - Adds a load event listener to fetch the details of the connected NFT
      */
     useEffect(() => {
-      window.addEventListener("load", getDetails);
+        window.addEventListener("load", getDetails);
     });
-  
+
     /**
      * Gets the details of the connected NFT, found within the url.
      * A valid NFT collection address and tokenID are expected within
@@ -106,78 +106,79 @@ function Sell() {
      *
      * Will fetch the data needed from the opensea API and update the page.
      */
-     async function getDetails(){
-      let urlParts = window.location.pathname.split('/');
-      const [collectionAddr, tokenID] = urlParts.splice(-2);
-  
-      fetch(`${API_URL}/asset/${collectionAddr}/${tokenID}`, {method: "GET"})
-        .then((res) => res.json())
-        .then((json) => updateDetails(json))
-        .catch((err) => console.error(err));
+    async function getDetails() {
+        let urlParts = window.location.pathname.split('/');
+        const [collectionAddr, tokenID] = urlParts.splice(-2);
+
+        fetch(`${API_URL}/asset/${collectionAddr}/${tokenID}`, { method: "GET" })
+            .then((res) => res.json())
+            .then((json) => updateDetails(json))
+            .catch((err) => console.error(err));
     }
-  
+
     /**
    * Updates the page with the new token details
    *
    * @param tokenData {Object} the parsed JSON object retrieved after fetching
    * details from the opensea API.
    */
-    async function updateDetails(tokenData){
-      setTokenName(tokenData.name)
-      setTokenCollection(tokenData.collection.name);
-      setImgUrl(tokenData.image_url);
-      setSchemaName(tokenData.asset_contract.schema_name);
-      setTokenOwnerId(tokenData.top_ownerships[0].owner.address);
-  
-      if(tokenData.orders.length > 0){
-        setTokenPrice(tokenData.orders[0].base_price * Math.pow(10, -18));
-      }
-  
-      console.log(tokenData);
+    async function updateDetails(tokenData) {
+        setTokenName(tokenData.name)
+        setTokenCollection(tokenData.collection.name);
+        setImgUrl(tokenData.image_url);
+        setSchemaName(tokenData.asset_contract.schema_name);
+        setTokenOwnerId(tokenData.top_ownerships[0].owner.address);
+
+        if (tokenData.orders.length > 0) {
+            setTokenPrice(tokenData.orders[0].base_price * Math.pow(10, -18));
+        }
+
+        console.log(tokenData);
     }
 
-    const[data, setData] = useState(null)
-    const[method, setMethod] = useState('set')
-    const[bid, setBid] = useState(null)
-    const[reserved, setReserved] = useState(null)
-    const[expireDate, setExpireDate] = useState(null)
-    const[message, setMessage] = useState("Invalid input");
+    const [data, setData] = useState(null)
+    const [method, setMethod] = useState('set')
+    const [bid, setBid] = useState(null)
+    const [reserved, setReserved] = useState(null)
+    const [expireDate, setExpireDate] = useState(null)
+    const [message, setMessage] = useState("Invalid input");
     // const[selectedDate, setSelectedDate] = useState(null)
     // const[datetime, setDatetime] = useState('')
 
-    function changeData(val){
+    function changeData(val) {
         setData(val.target.value);
     }
 
-    function changeSellMethod(val){
+    function changeSellMethod(val) {
         setMethod(val);
     }
 
-    function changeBid(val){
+    function changeBid(val) {
         setBid(val.target.value);
     }
 
-    function changeReserved(val){
+    function changeReserved(val) {
         setReserved(val.target.value);
     }
 
-    async function makeSellOrder(){
+    async function makeSellOrder() {
 
         const seaport = await getOpenSeaPort()
-     
+
         let urlParts = window.location.pathname.split('/');
         const [tokenAddress, tokenId] = urlParts.splice(-2); //fetch token address + token ID from URL
-    
+
         let userInfo = JSON.parse(getCookie("uid"));
         const accountAddress = userInfo["walletAddress"];
-    
-        let asset = {tokenId, tokenAddress};
+
+        let asset = { tokenId, tokenAddress };
         // if (schemaName === "ERC1155") {asset["schemaName"] = "ERC1155"};
-    
+
         const listing = await seaport.createSellOrder({
-        asset,
-        accountAddress,
-        startAmount: getSalePrice()})
+            asset,
+            accountAddress,
+            startAmount: getSalePrice()
+        })
 
         document.getElementById("sellButton").innerHTML = "NFT listed for sale";
     }
@@ -226,26 +227,26 @@ function Sell() {
         });
     }
 
-    async function getOpenSeaPort(){
+    async function getOpenSeaPort() {
         const provider = await detectEthereumProvider();
         return new OpenSeaPort(provider, {
             networkName: Network.Rinkeby
         });
     }
 
-    function getSalePrice(){
+    function getSalePrice() {
         return Number(document.getElementById("salePrice").value);
     }
-    
+
     /* function getExpirationTime() {
         return Number(Math.round(new Date(document.getElementById("expirationTime").value).getTime() / 1000));
     } */
 
     function setExpirationTime() {
         //console.log(document.getElementById("elogdate").value + "T" + document.getElementById("elogtime").value);
-        return Number(Math.round(new Date(document.getElementById("elogdate").value + "T" + document.getElementById("elogtime").value)/1000));
+        return Number(Math.round(new Date(document.getElementById("elogdate").value + "T" + document.getElementById("elogtime").value) / 1000));
     }
-    
+
     function getMinBid() {
         return Number(document.getElementById("min-bid").value);
     }
@@ -271,8 +272,8 @@ function Sell() {
     //     setDatetime(dt);
     // }
 
-    function handlePost(){
-        
+    function handlePost() {
+
     }
 
     return (
@@ -280,78 +281,98 @@ function Sell() {
             <div className="sellTokenInfo">
                 <h1 className="sellTokenName">{tokenName}</h1>
                 <p className="sellTokenCollection"><i>{tokenCollection}</i></p>
-                <img src={imgUrl} alt={"Asset Image"} className="SellImage"/>
-            </div>        
+                <img src={imgUrl} alt={"Asset Image"} className="SellImage" />
+            </div>
+
             <div className='sellpage-main'>
                 <div className='sellpage-top-set-price'>
-                    <h3 className='select-sell-methods'>Select your sell method</h3>
+                    <h3 className='select-your-sell-methods'>Select your sell method</h3>
                     <br />
-                    <div className='sell-methods'>
-                        <button className='sell-methods-items' onClick={()=>changeSellMethod('set')}>
-                            <h4>Set Price</h4>
-                            <p>Sell at a fixed price</p>
-                        </button>
-                        <button className='sell-methods-items' onClick={()=>changeSellMethod('bid')}>
-                            <h4>Highest Bid</h4>
-                            <p>Auction to the highest bidder</p>
-                        </button>
-                    </div>
+                    {
+                        method === 'set' &&
+                        <div>
+                            <div className='sell-methods'>
+                                <button className='selected-sell-methods-items' onClick={() => changeSellMethod('set')}>
+                                    <h4>Set Price</h4>
+                                    <p>Sell at a fixed price</p>
+                                </button>
+                                <button className='sell-methods-items' onClick={() => changeSellMethod('bid')}>
+                                    <h4>Highest Bid</h4>
+                                    <p>Auction to the highest bidder</p>
+                                </button>
+                            </div>
+                        </div>
+                    }
+                    {
+                        method === 'bid' &&
+                        <div>
+                            <div className='sell-methods'>
+                                <button className='sell-methods-items' onClick={() => changeSellMethod('set')}>
+                                    <h4>Set Price</h4>
+                                    <p>Sell at a fixed price</p>
+                                </button>
+                                <button className='selected-sell-methods-items' onClick={() => changeSellMethod('bid')}>
+                                    <h4>Highest Bid</h4>
+                                    <p>Auction to the highest bidder</p>
+                                </button>
+                            </div>
+                        </div>
+                    }
+                    <hr />
                     <div>
-                        {
-                            method==='set' && 
-                            <div>
-                                <hr />
-                                <div className='set-sell-price'>
-                                    <div className='set-sell-price-left'>
-                                        <h3 className='price'>Price</h3>
-                                        <p className='price-description'>Will be on sale until you transfer this item or cancel it.</p>
-                                    </div>
-                                    <div className='set-sell-price-right'>
-                                        <input type="number" placeholder=" Amount" id="salePrice" onChange={changeData}/>
-                                    </div>
+                    {
+                        method === 'set' &&
+                        <div>
+                            <div className='set-sell-price'>
+                                <div className='set-sell-price-left'>
+                                    <h3 className='price'>Price</h3>
+                                    <p className='price-description'>Will be on sale until you transfer this item or cancel it.</p>
+                                </div>
+                                <div className='set-sell-price-right'>
+                                    <input type="number" placeholder=" Amount" id="salePrice" onChange={changeData} />
                                 </div>
                             </div>
-                        }
-                        {
-                            method==='bid' && 
-                            <div className='auction'>
-                                <hr />
-                                <div className='minimum-bid'>
-                                    <div className='set-minimum-bid-left'>
-                                        <h3 className='minimum'>Minimum Bid</h3>
-                                        <p className='minimum-bid-description'>Set your public starting bid price.</p>
-                                    </div>
-                                    <div className='set-minimum-bid-right'>
-                                        <input type="number" placeholder=" Amount" id="min-bid" onChange={changeBid} />
-                                    </div>
+                        </div>
+                    }
+                    {
+                        method === 'bid' &&
+                        <div className='auction'>
+                            <div className='minimum-bid'>
+                                <div className='set-minimum-bid-left'>
+                                    <h3 className='minimum'>Minimum Bid</h3>
+                                    <p className='minimum-bid-description'>Set your public starting bid price.</p>
                                 </div>
-                                <hr />
-                                <div className='reserve-price'>
-                                    <div className='reserve-price-left'>
-                                        <h3 className='reserve'>Reserve price</h3>
-                                        <p className='reserve-price-description'>Create a hidden limit by setting a reserve price.</p>
-                                    </div>
-                                    <div className='reserve-price-right'>
-                                        <input type="number" placeholder=" Amount" id="reserve-p" onChange={changeReserved} />
-                                    </div>
+                                <div className='set-minimum-bid-right'>
+                                    <input type="number" placeholder=" Amount" id="min-bid" onChange={changeBid} />
                                 </div>
-                                <hr />
-                                <div className='expiration-date'>
-                                    <div className='expiration-date-left'>
-                                        <h3 className='expire-date'>Expiration Date</h3>
-                                        <p className='expiration-date-description'>Your auction will automatically end at this time and the highest bidder will win. No need to cancel it!</p>
-                                    </div>
-                                    <div className='expiration-date-right'>
-                                        {/* <input type="datetime-local" className="expiration-date-time"
+                            </div>
+                            <hr />
+                            <div className='reserve-price'>
+                                <div className='reserve-price-left'>
+                                    <h3 className='reserve'>Reserve price</h3>
+                                    <p className='reserve-price-description'>Create a hidden limit by setting a reserve price.</p>
+                                </div>
+                                <div className='reserve-price-right'>
+                                    <input type="number" placeholder=" Amount" id="reserve-p" onChange={changeReserved} />
+                                </div>
+                            </div>
+                            <hr />
+                            <div className='expiration-date'>
+                                <div className='expiration-date-left'>
+                                    <h3 className='expire-date'>Expiration Date</h3>
+                                    <p className='expiration-date-description'>Your auction will automatically end at this time and the highest bidder will win. No need to cancel it!</p>
+                                </div>
+                                <div className='expiration-date-right'>
+                                    {/* <input type="datetime-local" className="expiration-date-time"
                                             value={(datetime || '').toString().substring(0, 16)}
                                             onChange={changeDateTime} /> */}
-                                        <ElogDateTime handleChange={(val) => {
-                                            setExpireDate(val);
-                                        }} />
-                                    </div>
+                                    <ElogDateTime handleChange={(val) => {
+                                        setExpireDate(val);
+                                    }} />
                                 </div>
                             </div>
-                        }
+                        </div>
+                    }
                     </div>
                 </div>
 
@@ -361,16 +382,15 @@ function Sell() {
                     <hr />
                     <div className='listing-section'>
                         <h3 className='listing'>Listing</h3>
-            
                         {
-                            method==='set' &&
+                            method === 'set' &&
 
                             <div>
-                                { 
+                                {
                                 // let sellDescription = ({data}===null) ?
                                 //     "Invalid price." : 
                                 //     "Your item will be listed for ${data}"
-                                    
+
                                 //     /* ({data}===null) ?
                                 //         (<p className='error-msg'>Invalid price.</p>) : 
                                 //         (<p className='listing-description'>Your item will be listed for {data}</p)> */
@@ -382,13 +402,13 @@ function Sell() {
                             </div>
                         }
                         {
-                            method==='bid' &&
+                            method === 'bid' &&
                             <div>
                                 {/* { 
-                                let bidDescription = ({bid}===null) ?
-                                    "Invalid price." : 
-                                    "Your item will be listed for ${bid}"
-                                } */}
+                                    let bidDescription = ({bid}===null) ?
+                                        "Invalid price." : 
+                                        "Your item will be listed for ${bid}"
+                                    }  */}
                                 <p className='listing-description'>Your item will be auctioned.
                                 The highest bidder will win it on {expireDate}, as long as their bid is at least {reserved}</p>
                                 <button className='post-button' onClick={() => makeAscendingAuction()}>Post your listing</button>
