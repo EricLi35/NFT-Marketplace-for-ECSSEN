efd8de5c8e647f9015fd467656914e6433677ad0
e seng <eth.sengsavang@gmail.com>
2021-07-20 14:24:23 -0600

    Add page to link to the Defi page

---
+++ src/components/DefiLink/index.js
@@ -0,0 +1,19 @@
+/**
+ * @author Ethan Sengsavang
+ *
+ * @version 2021.07.20 - Base development
+ * @since 2021.07.20
+ */
+
+import React from "react";
+
+const DefiLink = () => {
+  return(
+    <div className="DefiLinkPage">
+      <h1>Link to DeFi Page</h1>
+      <a href="https://bcharityecssen.netlify.app">
+        <p>Support our token!</p>
+      </a>
+    </div>
+  )
+}
