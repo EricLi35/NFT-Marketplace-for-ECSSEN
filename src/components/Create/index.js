ec83d9e7e7abfef69d39ec1650ebc1110e3d14cb
e seng <eth.sengsavang@gmail.com>
2021-07-20 11:18:52 -0600

    Move create body into return statement

--- src/components/Create/index.js
+++ src/components/Create/index.js
@@ -169,104 +169,106 @@ const Create = () => {
     }
 
     const renderCreateBody = () => {
-      <div className = "createThing">
- 
-        <div className = "createNewItem_text">
-            <h1>Create new item</h1>
-        </div>
-
-        <div className="file_types">
-        {/* <h4> */}
-        <strong className = "file_descrip">Use an image as your NFT</strong>
-        <div className="file_descrip_detailed">
-        File types supported: JPG, PNG, JPEG
-        </div>
-        {/* </h4> */}
-        </div>
-
-        <div className="App2">
-          <div className="container" onDrop={handleImageChange} onDragOver={dragOverHandler}>
-            {error && <p className="errorMsg">File not supported</p>}
-            <div className="imgPreview"
-            style= {{background: imgPreview ? `url("${imgPreview}")no-repeat center/cover` : "transparent"}}
-            >
-              {!imgPreview && (
-                <>
-                <p></p>
-                <label htmlFor="fileUpload" className="customFileUpload">
-                  Drag & drop file
-                  <div>
-                    or <span className="fileTypeDescription">browse media on your device</span>
-                  </div>  
-                </label>
-                <input type="file" id="fileUpload" onChange={handleImageChange} />
-                </>
-              )}
-            </div>
-            {imgPreview && (
-              <button className="removeButton" onClick={() => setImgPreview(null)}>Remove image</button>
-            )}
-          </div>
-        </div>
-
-        <form>
-          <br></br><br></br>
-          <div className = "name">
-          {/* <div className = "name_text"> */}
-          <strong>Name <span className="asterisk">*</span></strong> <br></br>
-          {/* </div> */}
-
-          {/* <div className = "name_textbox"> */}
-          <input className="name_textbox_size" id="nameField" type="text" placeholder="Item Name"></input>
-          {/* </div> */}
-          
+      return(
+        <div className = "createThing">
+   
+          <div className = "createNewItem_text">
+              <h1>Create new item</h1>
           </div>
 
-          <br />
-
-          <div className="tags">
-            <strong>Item tags</strong>
-            <p>Tags should be separated by commas (,)</p>
-            <input className="name_textbox_size" type="text" placeholder="Art, Abstract, Colourful, ..." />
-          </div>
-
-          <br />
-          
-          {/* <h4 className = "description"> */}
-          <div className = "description">
-          <strong>Description</strong><br></br>
-
-        
-          <p className="description_text">
-          The description will be included on the item's detail page underneath its image.
-          {/*
-            <span style= {{color:"blue" }} > Markdown </span>
-            syntax is supported.
-          */}
-          </p>
-          <textarea className="description_textbox" name="comment[body]" rows="1" cols="50" wrap="physical" id="descriptionField" placeholder="Provide a detailed description of your item."></textarea>
-
+          <div className="file_types">
+          {/* <h4> */}
+          <strong className = "file_descrip">Use an image as your NFT</strong>
+          <div className="file_descrip_detailed">
+          File types supported: JPG, PNG, JPEG
           </div>
           {/* </h4> */}
+          </div>
 
-        </form>
-        <div className="DonateTransactionDetails">
-        {
-          progress > 0
-          ? <ProgressBar completed={progress} bgcolor={progressBg} />
-          : <></>
-        }
-        {
-          transactionHash !== ""
-          ? <a href={`${ETHERSCAN_URL}/tx/${transactionHash}`}>View your transaction</a>
-          : <p></p>
-        }
+          <div className="App2">
+            <div className="container" onDrop={handleImageChange} onDragOver={dragOverHandler}>
+              {error && <p className="errorMsg">File not supported</p>}
+              <div className="imgPreview"
+              style= {{background: imgPreview ? `url("${imgPreview}")no-repeat center/cover` : "transparent"}}
+              >
+                {!imgPreview && (
+                  <>
+                  <p></p>
+                  <label htmlFor="fileUpload" className="customFileUpload">
+                    Drag & drop file
+                    <div>
+                      or <span className="fileTypeDescription">browse media on your device</span>
+                    </div>  
+                  </label>
+                  <input type="file" id="fileUpload" onChange={handleImageChange} />
+                  </>
+                )}
+              </div>
+              {imgPreview && (
+                <button className="removeButton" onClick={() => setImgPreview(null)}>Remove image</button>
+              )}
+            </div>
+          </div>
+
+          <form>
+            <br></br><br></br>
+            <div className = "name">
+            {/* <div className = "name_text"> */}
+            <strong>Name <span className="asterisk">*</span></strong> <br></br>
+            {/* </div> */}
+
+            {/* <div className = "name_textbox"> */}
+            <input className="name_textbox_size" id="nameField" type="text" placeholder="Item Name"></input>
+            {/* </div> */}
+            
+            </div>
+
+            <br />
+
+            <div className="tags">
+              <strong>Item tags</strong>
+              <p>Tags should be separated by commas (,)</p>
+              <input className="name_textbox_size" type="text" placeholder="Art, Abstract, Colourful, ..." />
+            </div>
+
+            <br />
+            
+            {/* <h4 className = "description"> */}
+            <div className = "description">
+            <strong>Description</strong><br></br>
+
+          
+            <p className="description_text">
+            The description will be included on the item's detail page underneath its image.
+            {/*
+              <span style= {{color:"blue" }} > Markdown </span>
+              syntax is supported.
+            */}
+            </p>
+            <textarea className="description_textbox" name="comment[body]" rows="1" cols="50" wrap="physical" id="descriptionField" placeholder="Provide a detailed description of your item."></textarea>
+
+            </div>
+            {/* </h4> */}
+
+          </form>
+          <div className="DonateTransactionDetails">
+          {
+            progress > 0
+            ? <ProgressBar completed={progress} bgcolor={progressBg} />
+            : <></>
+          }
+          {
+            transactionHash !== ""
+            ? <a href={`${ETHERSCAN_URL}/tx/${transactionHash}`}>View your transaction</a>
+            : <p></p>
+          }
+          </div>
+          <button className="CreateButton" onClick={() => createNFT()} disabled={disableButton}>
+            <Plus className="CreatePlus" />
+            <p>Create Token</p>
+          </button>
         </div>
-        <button className="CreateButton" onClick={() => createNFT()} disabled={disableButton}>
-          <Plus className="CreatePlus" />
-          <p>Create Token</p>
-        </button>
-      </div>
+      );
       }
 
     return (
