8e5f63ccd1401aef99351d27bd4d9690a9c8bec9
lij313 <lij313@mcmaster.ca>
2021-07-29 16:55:28 -0600

    More links added to the about page

--- src/components/DefiLink/index.js
+++ src/components/DefiLink/index.js
@@ -11,11 +11,41 @@ import "./DefiLink.css";
 const DefiLink = () => {
   return(
     <div className="DefiLinkPage">
-      <div className="defiTextContainer">
-        <h1>Link to DeFi Page</h1>
-        <a href="https://bcharityecssen.netlify.app" target="_blank">
-          <p>Support our token!</p>
-        </a>
+      <div className="Aboutpage-header">
+        <h2>This is the abuot page</h2>
+      </div>
+
+      <hr />
+
+      <div className="links-container">
+        <div className="defiTextContainer">
+          <h1>Link to DeFi Page</h1>
+          <a href="https://bcharityecssen.netlify.app" target="_blank" clasName="link-items">
+            <p>Support our token!</p>
+          </a>
+        </div>
+
+        <div className="redditLinkContainer">
+          <h1>Link to Reddit</h1>
+          <a href="https://www.reddit.com/r/BCharity/" target="_blank" clasName="link-items">
+            <p>Connect us on Reddit!</p>
+          </a>
+        </div>
+
+        <div className="mediumLinkContainer">
+          <h1>Link to Medium</h1>
+          <a href="https://bcharityfi.medium.com" target="_blank" clasName="link-items">
+            <p>Connect us on Medium!</p>
+          </a>
+        </div>
+
+        <div className="telegramLinkContainer">
+          <h1>Link to Telegram</h1>
+          <a href="https://t.me/BCharitynet" target="_blank" clasName="link-items">
+            <p>Connect us on Telegram!</p>
+          </a>
+        </div>
+
       </div>
     </div>
   )
