4f3578b330a33deab771d5176e4ee32de84af327
e seng <eth.sengsavang@gmail.com>
2021-07-30 17:10:58 -0600

    Update how charity info is saved

--- src/components/Donate/DonateInfo.js
+++ src/components/Donate/DonateInfo.js
@@ -1,20 +1,8 @@
-const donateData = [
-    {
-        id: 1, 
-        charityName: "ECSSEN Career School" ,
-        address: " 0xE8B0F3a71edFF410a82B0c4FcD2C4aD1a94d0Ee2", 
-    },
-    {
-        id: 2, 
-        charityName: "United Way" ,
-        address: "*address of United Way*", 
-    },
-    {
-        id: 3, 
-        charityName: "Children Hospital" ,
-        address: "*address of Children Hospital*", 
-    }
-
-]
+const donateData = {
+    "ECSSEN Career School": " 0xE8B0F3a71edFF410a82B0c4FcD2C4aD1a94d0Ee2", 
+    "United Way": "0xE8B0F3a71edFF410a82B0c4FcD2C4aD1a94d0Ee2", // same as ecssen carrer school, temp 
+    "Children Hospital": "0xE8B0F3a71edFF410a82B0c4FcD2C4aD1a94d0Ee2", // same as ecssen carrer school, temp
+    "Test": "0x1437B4031f2b6c3a6d1D5F68502407283c3fAE31"
+}
 
 export default donateData
