bffc3efa5a32f5ad19d52f23240c738bd0242928
e seng <eth.sengsavang@gmail.com>
2021-07-30 17:22:10 -0600

    Remove test wallet address

--- src/components/Donate/DonateInfo.js
+++ src/components/Donate/DonateInfo.js
@@ -2,7 +2,6 @@ const donateData = {
     "ECSSEN Career School": " 0xE8B0F3a71edFF410a82B0c4FcD2C4aD1a94d0Ee2", 
     "United Way": "0xE8B0F3a71edFF410a82B0c4FcD2C4aD1a94d0Ee2", // same as ecssen carrer school, temp 
     "Children Hospital": "0xE8B0F3a71edFF410a82B0c4FcD2C4aD1a94d0Ee2", // same as ecssen carrer school, temp
-    "Test": "0x1437B4031f2b6c3a6d1D5F68502407283c3fAE31"
 }
 
 export default donateData
